6f29a32963a5bf589c098636373a70b261795f5b
sanakarnelia <sanakarnelia@gmail.com>
2025-10-30 15:31:02 -0400

    create a second activity and connect it with main activity

--- app/src/main/java/com/example/timestable/MainActivity.java
+++ app/src/main/java/com/example/timestable/MainActivity.java
@@ -1,8 +1,11 @@
 package com.example.timestable;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -14,6 +17,8 @@ public class MainActivity extends AppCompatActivity {
 
     Button tableBn,HistoryBtn;
     EditText userInput;
+
+    ListView listView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,6 +28,26 @@ public class MainActivity extends AppCompatActivity {
         userInput = findViewById(R.id.userIn);
         tableBn = findViewById(R.id.tableBtn);
         HistoryBtn = findViewById(R.id.historybtn);
+
+        //start second activity (History view)when user click on the history button
+        HistoryBtn.setOnClickListener( v ->{
+            Intent intent = new Intent(MainActivity.this, MainActivity2.class);
+            startActivity(intent);
+        });
+
+
+        //when user click on table button
+        tableBn.setOnClickListener(v -> calculateTable());
+    }
+    public void calculateTable(){
+       String number = userInput.getText().toString();
+
+        //check if user input is not null
+        if(number.isEmpty()){
+            Toast.makeText(this,"plz Enter A number",Toast.LENGTH_SHORT).show();
+        }
+
+
 
     }
 }
