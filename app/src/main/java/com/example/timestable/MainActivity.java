13e09ed613d6d00fa74c9a4da2db0357c162374e
sanakarnelia <sanakarnelia@gmail.com>
2025-10-30 15:42:32 -0400

    calculate table

--- app/src/main/java/com/example/timestable/MainActivity.java
+++ app/src/main/java/com/example/timestable/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.timestable;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
@@ -13,12 +14,19 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
 
     Button tableBn,HistoryBtn;
     EditText userInput;
 
     ListView listView;
+
+    private ArrayList<String> TableList = new ArrayList<>();
+    private static ArrayList<Integer> HistoryList = new ArrayList<>();
+    private ArrayAdapter<String> adapter;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -28,10 +36,15 @@ public class MainActivity extends AppCompatActivity {
         userInput = findViewById(R.id.userIn);
         tableBn = findViewById(R.id.tableBtn);
         HistoryBtn = findViewById(R.id.historybtn);
+        listView = findViewById(R.id.listview);
+
+        adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1,TableList);
+        listView.setAdapter(adapter);
 
         //start second activity (History view)when user click on the history button
         HistoryBtn.setOnClickListener( v ->{
             Intent intent = new Intent(MainActivity.this, MainActivity2.class);
+            intent.putIntegerArrayListExtra("HistoryList",HistoryList);
             startActivity(intent);
         });
 
@@ -45,7 +58,21 @@ public class MainActivity extends AppCompatActivity {
         //check if user input is not null
         if(number.isEmpty()){
             Toast.makeText(this,"plz Enter A number",Toast.LENGTH_SHORT).show();
+            return;
         }
+        //Calculate table
+        int Tnumber = Integer.parseInt(number);
+        for( int i=1;i<=10;i++){
+            TableList.add(Tnumber + " X " + i +" = " + (Tnumber*i));
+
+        }
+        adapter.notifyDataSetChanged();
+
+
+        if(!HistoryList.contains(Tnumber)){
+            HistoryList.add(Tnumber);
+        }
+
 
 
 
